025e1706936942be4866a1e46428cf2d77fd4e0a
Joshua Rolstad <jrolstad@gmail.com>
2023-10-23 16:29:49 -0700

    Stubbing out infra

---
+++ deployments/infra/glue_db_tables.tf
@@ -0,0 +1,31 @@
+resource "aws_glue_catalog_table" "observation" {
+  name          = "observation"
+  database_name = aws_glue_catalog_database.object_db.name
+
+  table_type = "EXTERNAL_TABLE"
+
+  parameters = {
+    EXTERNAL              = "TRUE"
+  }
+
+  storage_descriptor {
+    location      = "s3://${local.observation_bucket_name}/data"
+    input_format  = "org.apache.hadoop.mapred.TextInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+
+    ser_de_info {
+      name                  = "json"
+      serialization_library = "org.openx.data.jsonserde.JsonSerDe"
+
+      parameters = {
+        "serialization.format" = 1
+      }
+    }
+
+    columns {
+      name = "date"
+      type = "datetime"
+    }
+
+  }
+}
