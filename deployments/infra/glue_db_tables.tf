8cdf9bbcdfe86a295709a28beb5a369e774fa4de
Joshua Rolstad <jrolstad@gmail.com>
2023-10-23 17:32:59 -0700

    Adding extract columns to table

--- deployments/infra/glue_db_tables.tf
+++ deployments/infra/glue_db_tables.tf
@@ -5,7 +5,8 @@ resource "aws_glue_catalog_table" "observation" {
   table_type = "EXTERNAL_TABLE"
 
   parameters = {
-    EXTERNAL              = "TRUE"
+    "classification"         = "csv"
+    "skip.header.line.count" = "6"
   }
 
   storage_descriptor {
@@ -14,17 +15,113 @@ resource "aws_glue_catalog_table" "observation" {
     output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
 
     ser_de_info {
-      name                  = "json"
-      serialization_library = "org.openx.data.jsonserde.JsonSerDe"
+      name                  = "csv"
+      serialization_library = "org.apache.hadoop.hive.serde2.OpenCSVSerde"
 
       parameters = {
-        "serialization.format" = 1
+        "separatorChar" = ","
       }
     }
 
     columns {
       name = "date"
-      type = "datetime"
+      type = "string"
+    }
+    columns {
+      name = "time"
+      type = "string"
+    }
+    columns {
+      name = "system_setting"
+      type = "string"
+    }
+    columns {
+      name = "system_mode"
+      type = "string"
+    }
+    columns {
+      name = "calendar_event"
+      type = "string"
+    }
+    columns {
+      name = "program_mode"
+      type = "string"
+    }
+    columns {
+      name = "cool_set_temp"
+      type = "string"
+    }
+    columns {
+      name = "heat_set_temp"
+      type = "string"
+    }
+    columns {
+      name = "current_temp"
+      type = "string"
+    }
+    columns {
+      name = "current_humidity"
+      type = "string"
+    }
+    columns {
+      name = "outdoor_temp"
+      type = "string"
+    }
+    columns {
+      name = "outdoor_wind_speed"
+      type = "string"
+    }
+    columns {
+      name = "cool_stage1_seconds"
+      type = "string"
+    }
+    columns {
+      name = "heat_stage1_seconds"
+      type = "string"
+    }
+    columns {
+      name = "fan_seconds"
+      type = "string"
+    }
+    columns {
+      name = "dm_offset"
+      type = "string"
+    }
+    columns {
+      name = "thermostat_temp"
+      type = "string"
+    }
+    columns {
+      name = "thermostat_humidity"
+      type = "string"
+    }
+    columns {
+      name = "thermostat_motion"
+      type = "string"
+    }
+    columns {
+      name = "sensor1_temp"
+      type = "string"
+    }
+    columns {
+      name = "sensor1_motion"
+      type = "string"
+    }
+    columns {
+      name = "sensor2_temp"
+      type = "string"
+    }
+    columns {
+      name = "sensor2_motion"
+      type = "string"
+    }
+    columns {
+      name = "sensor3_temp"
+      type = "string"
+    }
+    columns {
+      name = "sensor3_motion"
+      type = "string"
     }
 
   }
--- deployments/infra/locals.tf
+++ deployments/infra/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  service_name = "thermostat_insights_${var.environment}"
-  observation_bucket_name = replace("${local.service_name}observationdata","_","")
-  athena_bucket_name = replace("${local.service_name}athena","_","")
+  service_name            = "thermostat_insights_${var.environment}"
+  observation_bucket_name = replace("${local.service_name}observationdata", "_", "")
+  athena_bucket_name      = replace("${local.service_name}athena", "_", "")
 }
